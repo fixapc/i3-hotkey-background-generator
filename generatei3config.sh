b7fbd5dc42135f23dc687595276d082f301d9c39
dustin@fixapc.net <Lucky07310731###>
2022-10-31 17:34:32 +0000

    push

--- generatei3config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Heres to hope
-cat /home/usr/.config/i3/config | grep -i -o "\$.*I3BACKGROUNDGENERATOR.*" | sed 's&exec.* # I3BACKGROUNDGENERATOR:&&gI'| sed 's&$mod&Win&gI' |  sed 's&[$]&&gI'
